80781c121ee339c2609fe78fcf76a5b3b0483624
sumitbsp <sumit.bsp88@gmail.com>
2020-06-17 04:06:48 +0530

    Updates

---
+++ precache-manifest.e92ff9a0aacc44911e997103516dc224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b048a6683ff36bd7c863be2a7341dd",
+    "url": "/sumitbsp/where-in-the-world-react/index.html"
+  },
+  {
+    "revision": "711ac5d8b408956ba91c",
+    "url": "/sumitbsp/where-in-the-world-react/static/css/main.28a1438f.chunk.css"
+  },
+  {
+    "revision": "5da1588e5b98f7b65d6c",
+    "url": "/sumitbsp/where-in-the-world-react/static/js/2.0096f29b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sumitbsp/where-in-the-world-react/static/js/2.0096f29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711ac5d8b408956ba91c",
+    "url": "/sumitbsp/where-in-the-world-react/static/js/main.a2bbb4ba.chunk.js"
+  },
+  {
+    "revision": "0884cdb36546ea9c6897",
+    "url": "/sumitbsp/where-in-the-world-react/static/js/runtime-main.dd2a3746.js"
+  }
+]);
