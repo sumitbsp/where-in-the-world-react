55ff6c623cd8c11ecbe1b24d7fe6494659773888
sumitbsp <sumit.bsp88@gmail.com>
2020-06-18 01:01:51 +0530

    Updates

---
+++ precache-manifest.fc9101104744b8643d52440ee26a29ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725f40f114e83301b6b332d87d29b8f3",
+    "url": "/where-in-the-world-react/index.html"
+  },
+  {
+    "revision": "4679f0b482f4cf7cd99d",
+    "url": "/where-in-the-world-react/static/css/2.03536927.chunk.css"
+  },
+  {
+    "revision": "73a39cb753e08982d3ce",
+    "url": "/where-in-the-world-react/static/css/main.54ffcfa8.chunk.css"
+  },
+  {
+    "revision": "4679f0b482f4cf7cd99d",
+    "url": "/where-in-the-world-react/static/js/2.2fc90978.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/where-in-the-world-react/static/js/2.2fc90978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a39cb753e08982d3ce",
+    "url": "/where-in-the-world-react/static/js/main.36b915f7.chunk.js"
+  },
+  {
+    "revision": "bbac1b28d77b78eed355",
+    "url": "/where-in-the-world-react/static/js/runtime-main.86ec21e7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/where-in-the-world-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/where-in-the-world-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/where-in-the-world-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/where-in-the-world-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/where-in-the-world-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sumitbsp/where-in-the-world-react/precache-manifest.e92ff9a0aacc44911e997103516dc224.js"
+  "/where-in-the-world-react/precache-manifest.fc9101104744b8643d52440ee26a29ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sumitbsp/where-in-the-world-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/where-in-the-world-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
